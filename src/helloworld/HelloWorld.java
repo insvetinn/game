d373d544bdb0a8d581a295ba797c48c3c87a725f
linglan <71087538+insvetinn@users.noreply.github.com>
2022-06-04 09:59:37 +0800

    Initial commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class HelloWorld {
+	protected static void sayHello() {
+		System.out.println("Hello, World! ");
+	}
+}
---
+++ test/helloworld/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+	@Test
+	public void test() {
+		HelloWorld.sayHello();
+	}
+}
